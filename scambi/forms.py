074a5eabbb6cb6b6141cb65e56842088f282a853
Tommaso Cus <tommasocus@Tommasos-MBP.station>
2025-09-21 08:53:35 +0200

    Fix definitivo: Corretto forms.py con indentazione corretta

--- scambi/forms.py
+++ scambi/forms.py
@@ -1,10 +1,19 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-
-class CustomUserCreationForm(UserCreationForm):
-   
 from django import forms
 from .models import Annuncio, Categoria
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ("username", "email", "password1", "password2")
+        
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['username'].widget.attrs.update({'class': 'form-control'})
+        self.fields['email'].widget.attrs.update({'class': 'form-control'})
+        self.fields['password1'].widget.attrs.update({'class': 'form-control'})
+        self.fields['password2'].widget.attrs.update({'class': 'form-control'})
 
 class AnnuncioForm(forms.ModelForm):
     class Meta:
@@ -16,10 +25,10 @@ class AnnuncioForm(forms.ModelForm):
             'categoria': forms.Select(attrs={'class': 'form-control'}),
             'tipo': forms.Select(attrs={'class': 'form-control'}),
             'immagine': forms.FileInput(attrs={
-    'class': 'form-control', 
-    'accept': 'image/*',
-    'onchange': 'previewImage(this);'
-}),
+                'class': 'form-control', 
+                'accept': 'image/*',
+                'onchange': 'previewImage(this);'
+            }),
         }
         labels = {
             'titolo': 'Titolo annuncio',
